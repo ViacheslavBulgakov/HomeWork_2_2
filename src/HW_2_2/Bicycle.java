be38da61e887d431f99372e0f68d689df345147b
kkurs@mail.ru <kkurs@mail.ru>
2024-04-01 23:25:22 +0700

    ДЗ ООП Часть2. Изначальная программа.

---
+++ src/HW_2_2/Bicycle.java
@@ -0,0 +1,11 @@
+package HW_2_2;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/HW_2_2/ServiceStation.java
@@ -0,0 +1,28 @@
+package HW_2_2;
+
+import HW_2_2.Bicycle;
+import HW_2_2.Car;
+
+public class ServiceStation {
+    public void check(Car car, Bicycle bicycle, Truck truck) {
+        if (car != null) {
+            System.out.println("Обслуживаем " + car.modelName);
+            for (int i = 0; i < car.wheelsCount; i++) {
+                car.updateTyre();
+            }
+            car.checkEngine();
+        } else if (truck != null) {
+            System.out.println("Обслуживаем " + truck.modelName);
+            for (int i = 0; i < truck.wheelsCount; i++) {
+                truck.updateTyre();
+            }
+            truck.checkEngine();
+            truck.checkTrailer();
+        } else if (bicycle != null) {
+            System.out.println("Обслуживаем " + bicycle.modelName);
+            for (int i = 0; i < bicycle.wheelsCount; i++) {
+                bicycle.updateTyre();
+            }
+        }
+    }
+}
